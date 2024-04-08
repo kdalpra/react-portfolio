9fef974aba4eaa491a2c78245143dc4c0d5b0173
Klay <55288045+kdalpra@users.noreply.github.com>
2024-04-08 02:55:30 -0400

    added app ads

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="content">
+        <div className="top-container">
+          <nav className="navbar">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        </div>
+        
+        <div className="bottom-container">
+          <div className="left-side">
+            <h1>Title 1</h1>
+            <p>This is a paragraph of text under the title on the left side...</p>
+          </div>
+          <div className="right-side">
+            <h1>Title 2</h1>
+            <ul>
+              <li>Project 1</li>
+              <li>Project 2</li>
+              <li>Project 3</li>
+            </ul>
+          </div>
+          <div className="new-div">
+          {/* This is the new div. You can add content here if needed. */}
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
